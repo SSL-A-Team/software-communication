f8a79bd8c9462224ff08773c9a532ed305f39209
Austin Jones <austin.jones231@gmail.com>
2022-02-13 19:46:33 -0500

    Made it work

--- ai.py
+++ ai.py
@@ -1,137 +1,153 @@
 import socket, time, threading, random, logging
 
-HOST = '172.25.100.221'
-SRVPORT = 8081
-CLIPORT = 8080
-TIMEOUT = 30
-robots = []
-lock = threading.Lock()
-DATA_SIZE = 508
 
-def setKeepalive(addr):
-    for r in robots:
-        if r['addr'] == (addr[0], CLIPORT):
-            r['keepalive'] = time.time()
-            logging.debug(f"Set keepalive for {(addr[0], CLIPORT)}")
+class Ai():
 
-def handleRobot(d, addr, s):
-    data = bytearray(d)
-    cmd = data[0]
-    ret = bytearray(DATA_SIZE)
-    addr = (addr[0], CLIPORT)
-    logging.debug(f"Got traffic from {addr} {data[0]} {data[1]}")
+    def __init__(self):
+        self._host = '172.25.100.221'
+        self._srvport = 8081
+        self._cliport = 8080
+        self._timeout = 30
+        self._robots = []
+        self._lock = threading.Lock()
+        self._data_size = 508
+        self._socket = None
 
-    # Handle client Hello
-    if cmd == 219:
-        r = {'addr': addr, 'jersey': 0, 'keepalive': time.time()}
-        if r not in robots:
-            robots.append(r)
-            logging.debug(f"Added {r} to robot list")
-        else:
-            logging.debug(f"Got hello for {r} I already knew about")
-        ret[0] = 221
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            self._socket = s
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            self._socket.bind(("", self._srvport))
+            data = bytearray(self._data_size)
+            data[0] = 220
+            order_thread = threading.Thread(target=self.giveOrders)
+            order_thread.start()
+            keepalive_thread = threading.Thread(target=self.checkKeepalives)
+            keepalive_thread.start()
+            with self._lock:
+                self._socket.sendto(data, ('255.255.255.255', self._cliport))
+            logging.debug(f"Sent hello to broadcast")
+            while True:
+                data, addr = s.recvfrom(self._data_size+8)
+                handle_thread = threading.Thread(target=self.handleRobot, args=(data, addr))
+                handle_thread.start()
 
-    # Handle client jersey number
-    elif cmd == 221:
-        isIn = False
-        for r in robots:
-            if r['addr'] == addr:
-                r['jersey'] = data[1]
-                logging.debug(f"Set jersey to {data[1]} for {r['addr']}")
-                isIn = True
-        if not isIn:
-            r = {'addr': addr, 'jersey': data[1], 'keepalive': time.time()}
-            robots.append(r)
-            logging.debug(f"Added new robot {r}")
-        fail = False
-        for r in robots:
-            if r['jersey'] == data[1] and r['addr'] != addr:
-                fail = True
-                ret[0] = 255
-                e = bytearray(256)
-                e[0] = 255
-                with lock:
-                    s.sendto(e, r['addr'])  # Notify conflicting bot
-                logging.debug(f"Found conflicting jersey number {data[1]} between {r} and {addr}")
-        if not fail:
+    def setKeepalive(self, addr):
+        for r in self._robots:
+            if r['addr'] == (addr[0], self._cliport):
+                with self._lock:
+                    r['keepalive'] = time.time()
+                logging.debug(f"Set keepalive for {(addr[0], self._cliport)}")
+
+    def handleRobot(self, d, addr):
+        data = bytearray(d)
+        cmd = data[0]
+        ret = bytearray(self._data_size)
+        addr = (addr[0], self._cliport)
+        logging.debug(f"Got traffic from {addr} {data[0]} {data[1]}")
+
+        # Handle client Hello
+        if cmd == 219:
+            r = {'addr': addr, 'jersey': 0, 'keepalive': time.time()}
+            if r not in self._robots:
+                with self._lock:
+                    self._robots.append(r)
+                logging.debug(f"Added {r} to robot list")
+            else:
+                logging.debug(f"Got hello for {r} I already knew about")
+            ret[0] = 222
+
+        # Handle client jersey number
+        elif cmd == 221:
+            isIn = False
+            for r in self._robots:
+                if r['addr'] == addr:
+                    with self._lock:
+                        r['jersey'] = data[1]
+                    logging.debug(f"Set jersey to {data[1]} for {r['addr']}")
+                    isIn = True
+            if not isIn:
+                r = {'addr': addr, 'jersey': data[1], 'keepalive': time.time()}
+                with self._lock:
+                    self._robots.append(r)
+                logging.debug(f"Added new robot {r}")
+            fail = False
+            for r in self._robots:
+                if r['jersey'] == data[1] and r['addr'] != addr:
+                    fail = True
+                    ret[0] = 255
+                    e = bytearray(256)
+                    e[0] = 255
+                    with self._lock:
+                        self._socket.sendto(e, r['addr'])  # Notify conflicting bot
+                    logging.debug(f"Found conflicting jersey number {data[1]} between {r} and {addr}")
+            if not fail:
+                ret[0] = 250
+            
+        # Handle client ACK
+        elif cmd == 250:
+            isIn = False
+            for r in self._robots:
+                if r['addr'] == addr:
+                    isIn = True
+                    self.setKeepalive(addr)
+            if not isIn:
+                with self._lock:
+                    self._robots.append({"addr": addr, 'journal': 0, 'timeout': time.time()})
+                ret[0] = 222
+            else:
+                logging.debug(f"Got ack from {addr}")
+                return  # Do nothing and don't reply
+
+        # Handle client shutdown
+        elif cmd == 251:
+            for r in self._robots:
+                if r['addr'] == addr and r['jersey'] == data[1]:
+                    with self._lock:
+                        self._robots.remove(r)
             ret[0] = 250
-        
-    # Handle client ACK
-    elif cmd == 250:
-        isIn = False
-        for r in robots:
-            if r['addr'] == addr:
-                isIn = True
-                setKeepalive(addr)
-        if not isIn:
-            robots.append({"addr": addr, 'journal': 0, 'timeout': time.time()})
-            ret[0] = 221
+            self.setKeepalive(addr)
+            logging.debug(f"{addr} is shutting down")
+
+        # Handle client keepalive
+        elif cmd == 100:
+            ret[0] = 250
+            self.setKeepalive(addr)
+            logging.debug(f"Got keepalive from {addr}")
+
+        # Handle unsupported command
         else:
-            logging.debug(f"Got ack from {addr}")
-            return  # Do nothing and don't reply
+            ret[0] = 254
+            self.setKeepalive(addr)
+            logging.debug(f"Got unsupported command {cmd} from {addr}")
+        
+        with self._lock:
+            self._socket.sendto(ret, addr)
+            logging.debug(f"Sent {ret[0]} to {addr}")
 
-    # Handle client shutdown
-    elif cmd == 251:
-        for r in robots:
-            if r['addr'] == addr and r['jersey'] == data[1]:
-                robots.remove(r)
-        ret[0] = 250
-        setKeepalive(addr)
-        logging.debug(f"{addr} is shutting down")
+    def giveOrders(self):
+        # Wait for init
+        while self._robots == []:
+            logging.debug("Waiting for robots....")
+            time.sleep(1)
 
-    # Handle client keepalive
-    elif cmd == 100:
-        ret[0] = 250
-        setKeepalive(addr)
-        logging.debug(f"Got keepalive from {addr}")
+        # Send random data
+        for j in range(10):
+            ret = bytearray(self._data_size)
+            ret[0] = 125
+            for i in range(1,self._data_size-1):
+                ret[i] = random.getrandbits(8)
+            with self._lock:
+                self._socket.sendto(ret, self._robots[0]['addr'])   
 
-    # Handle unsupported command
-    else:
-        ret[0] = 254
-        setKeepalive(addr)
-        logging.debug(f"Got unsupported command {cmd} from {addr}")
-    
-    with lock:
-        s.sendto(ret, addr)
-        logging.debug(f"Sent {ret[0]} to {addr}")
-
-def giveOrders(s):
-    # Wait for init
-    while robots == []:
-        logging.debug("Waiting for robots....")
-        time.sleep(1)
-
-    # Send random data
-    for j in range(10):
-        ret = bytearray(DATA_SIZE)
-        ret[0] = 125
-        for i in range(1,DATA_SIZE-1):
-            ret[i] = random.getrandbits(8)
-        s.sendto(data, robots[0]['addr'])   
-
-def checkKeepalives():
-    while True:
-        for r in robots:
-            if time.time() - r['keepalive'] > 30:
-                robots.remove(r)
-                logging.debug(f"Pruned expired bot {r}")
-        time.sleep(TIMEOUT / 6)
+    def checkKeepalives(self):
+        while True:
+            for r in self._robots:
+                if time.time() - r['keepalive'] > 30:
+                    with self._lock:
+                        self._robots.remove(r)
+                    logging.debug(f"Pruned expired bot {r}")
+            time.sleep(self._timeout / 6)
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.bind(("", SRVPORT))
-    data = bytearray(DATA_SIZE)
-    data[0] = 220
-    order_thread = threading.Thread(target=giveOrders, args=(s,))
-    order_thread.start()
-    keepalive_thread = threading.Thread(target=checkKeepalives)
-    keepalive_thread.start()
-    with lock:
-        s.sendto(data, ('255.255.255.255', CLIPORT))
-    logging.debug(f"Sent hello to broadcast")
-    while True:
-        data, addr = s.recvfrom(DATA_SIZE+8)
-        handle_thread = threading.Thread(target=handleRobot, args=(data, addr, s))
-        handle_thread.start()
+a = Ai()
--- robot.py
+++ robot.py
@@ -1,90 +1,101 @@
 import socket, time, threading, logging
 
-HOST = '172.25.100.221'
-CLIPORT = 8080
-SRVPORT = 8081
-CLIENTS = []
-TIMEOUT = 30
-JERSEY = 1
-KEEPALIVE = 0
-SERVER = 0
-lock = threading.Lock()
-DATA_SIZE = 508
+class Robot():
 
-def handle(d, addr, s):
-    data = bytearray(d)
-    cmd = data[0]
-    logging.debug(f"Received {cmd} from {addr}")
-    ret = bytearray(DATA_SIZE)
-    if cmd == 219:
-        logging.debug(f"Traffic ignored from {addr}")
-        return  # Ignore broadcast traffic not from the server
+    def __init__(self):
+        self._host = '172.25.100.221'
+        self._cliport = 8080
+        self._srvport = 8081
+        self._timeout = 30
+        self._jersey = 1
+        self._keepalive = 0
+        self._server = ()
+        self._lock = threading.Lock()
+        self._data_size = 508
+        self._socket = None
 
-    SERVER = (addr[0], SRVPORT)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s: 
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            s.bind(("", self._cliport))
+            self._socket = s
+            data = bytearray(self._data_size)
+            data[0] = 219
+            with self._lock:
+                self._socket.sendto(data, ('255.255.255.255', self._srvport))
+            logging.debug("Sent hello to broadcast")
+            keepalive_thread = threading.Thread(target=self.keepalive)
+            keepalive_thread.start()
+            while True:
+                data, addr = s.recvfrom(self._data_size + 8)
+                handle_thread = threading.Thread(target=self.handle, args=(data, addr))
+                handle_thread.start()
+
+    def handle(self, d, addr):
+        s = self._socket
+        if not self._server:
+            self._server = ('0.0.0.0', self._srvport)
+        data = bytearray(d)
+        cmd = data[0]
+        logging.debug(f"Received {cmd} from {addr}")
+        ret = bytearray(self._data_size)
         
-    # Handle server ACK
-    if cmd == 250:
-        logging.debug(f"Received server ack")
+        # Handle server ACK
+        if cmd == 250:
+            logging.debug(f"Received server ack")
 
-    # Handle server hello
-    elif cmd == 220:
-        ret[0] = 250
-        logging.debug(f"Received hello from {SERVER}")
-        with lock:
-            KEEPALIVE = time.time()
-            s.sendto(ret, SERVER)
+        # Handle server hello
+        elif cmd == 220:
+            ret[0] = 250
+            with self._lock:
+                self._server = (addr[0], self._srvport)
+                logging.debug(f"Received hello from {self._server}")
+                self._keepalive = time.time()
+                s.sendto(ret, self._server)
 
-    elif cmd == 221:
-        ret[0] = 221
-        ret[1] = JERSEY
-        logging.debug(f"Provided jersey number {JERSEY} to {SERVER}")
-        with lock:
-            KEEPALIVE = time.time()
-            s.sendto(ret, SERVER)
+        elif cmd == 222:
+            ret[0] = 221
+            ret[1] = self._jersey
+            with self._lock:
+                self._server = (addr[0], self._srvport)
+                print(self._server)
+                logging.debug(f"Provided jersey number {self._jersey} to {self._server}")
+                self._keepalive = time.time()
+                s.sendto(ret, self._server)
 
-    elif cmd == 125:
-        ret[0] = 250
-        data.pop(0)
-        raw = bytes(data)
-        logging.debug(f"Received {raw}")
-        with lock:
-            KEEPALIVE = time.time()
-            s.sendto(ret, SERVER)
+        elif cmd == 125:
+            ret[0] = 250
+            data.pop(0)
+            raw = bytes(data)
+            logging.debug(f"Received {raw}")
+            with self._lock:
+                self._keepalive = time.time()
+                s.sendto(ret, self._server)
 
-    elif cmd == 255:
-        ret[0] == 250
-        logging.debug(f"Received server stop")
-        with lock:
-            KEEPALIVE = time.time()
-            s.sendto(ret, SERVER)
+        elif cmd == 255:
+            ret[0] == 250
+            logging.debug(f"Received server stop")
+            with self._lock:
+                self._keepalive = time.time()
+                s.sendto(ret, self._server)
 
-def keepalive(s):
-    while True:
-        if SERVER == 0:
-            time.sleep(TIMEOUT / 6)
-            continue
-        with lock:
-            if time.time() - KEEPALIVE > 30:
-                data = bytearray(DATA_SIZE)
-                data[0] = 100
-                s.sendto(data, SERVER)
-                logging.debug(f"Sent keepalive")
-        time.sleep(TIMEOUT / 6)
+        else:
+            logging.debug(f"Ignoring invalid traffic {cmd} from {addr}")
+
+    def keepalive(self):
+        s = self._socket
+        while True:
+            if self._server == ():
+                time.sleep(self._timeout / 6)
+                continue
+            with self._lock:
+                if time.time() - self._keepalive > 30:
+                    data = bytearray(self._data_size)
+                    data[0] = 100
+                    s.sendto(data, self._server)
+                    logging.debug(f"Sent keepalive")
+            time.sleep(self._timeout / 6)
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.bind(("", CLIPORT))
-    data = bytearray(DATA_SIZE)
-    data[0] = 219
-    with lock:
-        s.sendto(data, ('255.255.255.255', SRVPORT))
-    logging.debug("Sent hello to broadcast")
-    keepalive_thread = threading.Thread(target=keepalive, args=(s,))
-    keepalive_thread.start()
-    while True:
-        data, addr = s.recvfrom(DATA_SIZE + 8)
-        handle_thread = threading.Thread(target=handle, args=(data, addr, s))
-        handle_thread.start()
+r = Robot()
         
