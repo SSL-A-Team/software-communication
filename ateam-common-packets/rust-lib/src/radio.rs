12e4789593a4087b3cc4c130ffbed79322e1f710
Will Stuckey <guyfleeman@gmail.com>
2024-05-12 20:34:38 -0400

    add some gen2 packet wrappers

--- ateam-common-packets/rust-lib/src/radio.rs
+++ ateam-common-packets/rust-lib/src/radio.rs
@@ -1,7 +1,13 @@
 use crate::bindings_radio::{BasicControl, ParameterCommand};
 
-#[warn(dead_code)]
+#[derive(Copy, Clone)]
 pub enum DataPacket {
     BasicControl(BasicControl),
     ParameterCommand(ParameterCommand),
+}
+
+#[derive(Copy, Clone)]
+pub enum TelemetryPacket {
+    Basic(crate::bindings_radio::BasicTelemetry),
+    Control(crate::bindings_radio::ControlDebugTelemetry),
 }
