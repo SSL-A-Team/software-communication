1db8c233bef1c6eec904b916b19724a988465055
Joe Spall <jspall0327@gmail.com>
2025-02-02 21:23:17 -0800

    Update stspin.h

--- ateam-common-packets/include/stspin.h
+++ ateam-common-packets/include/stspin.h
@@ -3,9 +3,9 @@
  * @author Will Stuckey
  * @brief communication packet definitions for the stspin firmware
  * @version 0.1
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  * Struct sizes should pass static checks on the following 3 platforms:
  *  - Embedded -> C11, 4 byte ptr
  *  - Bindgen -> C11, 8 byte ptr
@@ -18,8 +18,8 @@
 #include "common.h"
 
 typedef enum MotorCommandPacketType {
-    MCP_PARAMS = 0,
-    MCP_MOTION = 1
+    MCP_PARAMS = 0x20,
+    MCP_MOTION = 0x21
 } __attribute__((packed)) MotorCommandPacketType;
 assert_size(MotorCommandPacketType, 1);
 
@@ -46,7 +46,9 @@ typedef struct MotorCommand_Params_Packet {
     float cur_i;
     float cur_d;
     float cur_i_max;
+
     uint16_t cur_clamp;
+    uint16_t reserved2;
     // add future params here
 } MotorCommand_Params_Packet;
 assert_size(MotorCommand_Params_Packet, 44);
@@ -55,25 +57,31 @@ typedef enum MotorCommand_MotionType {
     OPEN_LOOP = 0,
     VELOCITY = 1,
     TORQUE = 2,
-    BOTH= 3
-} MotorCommand_MotionType;
+    BOTH = 3
+} __attribute__((packed)) MotorCommand_MotionType;
+assert_size(MotorCommandPacketType, 1);
 
 typedef struct MotorCommand_Motion_Packet {
     uint32_t reset : 1;
     uint32_t enable_telemetry: 1;
     uint32_t reserved : 30;
+
     MotorCommand_MotionType motion_control_type;
+    uint8_t reserved2[3];
+
     float setpoint;
-} MotorCommand_Motion_Packet;
-assert_size(MotorCommand_Motion_Packet, 12);
+    uint32_t reserved3[8];
+} __attribute__((__packed__)) MotorCommand_Motion_Packet;
+assert_size(MotorCommand_Motion_Packet, 44); // Note: Same length as MotorCommand_Params_Packet
 
 typedef struct MotorCommandPacket {
     MotorCommandPacketType type;
+    uint8_t reserved[3];
     uint32_t crc32;
     union CommandData {
         MotorCommand_Params_Packet params;
         MotorCommand_Motion_Packet motion;
-    } data __attribute__((aligned (4)));
+    } data;
 } MotorCommandPacket;
 assert_size(MotorCommandPacket, 52);
 
@@ -82,8 +90,8 @@ assert_size(MotorCommandPacket, 52);
 /////////////////
 
 typedef enum MotorResponsePacketType {
-    MRP_PARAMS,
-    MRP_MOTION,
+    MRP_PARAMS = 0x80,
+    MRP_MOTION = 0x81,
 } __attribute__((packed)) MotorResponsePacketType;
 assert_size(MotorResponsePacketType, 1);
 
@@ -101,32 +109,33 @@ typedef struct MotorResponse_Params_Packet {
     float cur_i;
     float cur_d;
     float torque_i_max;
-    uint16_t cur_clamp;
 
+    uint16_t cur_clamp;
     uint16_t git_dirty: 1;
     uint16_t reserved: 15;
+    uint32_t reserved2;
 } __attribute__((packed)) MotorResponse_Params_Packet;
-assert_size(MotorResponse_Params_Packet, 44);
+assert_size(MotorResponse_Params_Packet, 48); // Note: Same length as MotorResponse_Params_Packet
 
 typedef struct MotorResponse_Motion_Packet {
-    uint8_t master_error : 1;
-    uint8_t hall_power_error : 1;
-    uint8_t hall_disconnected_error : 1;
-    uint8_t bldc_transition_error : 1;
-    uint8_t bldc_commutation_watchdog_error : 1;
-    uint8_t enc_disconnected_error: 1;
-    uint8_t enc_decoding_error : 1;
-    uint8_t hall_enc_vel_disagreement_error: 1;
-    uint8_t overcurrent_error : 1;
-    uint8_t undervoltage_error : 1;
-    uint8_t overvoltage_error : 1;
-    uint8_t torque_limited : 1;
-    uint8_t control_loop_time_error: 1;
-    uint8_t reset_watchdog_independent: 1;
-    uint8_t reset_watchdog_window: 1;
-    uint8_t reset_low_power: 1;
-    uint8_t reset_software: 1;
-    uint8_t reset_pin: 1;
+    uint32_t master_error : 1;
+    uint32_t hall_power_error : 1;
+    uint32_t hall_disconnected_error : 1;
+    uint32_t bldc_transition_error : 1;
+    uint32_t bldc_commutation_watchdog_error : 1;
+    uint32_t enc_disconnected_error: 1;
+    uint32_t enc_decoding_error : 1;
+    uint32_t hall_enc_vel_disagreement_error: 1;
+    uint32_t overcurrent_error : 1;
+    uint32_t undervoltage_error : 1;
+    uint32_t overvoltage_error : 1;
+    uint32_t torque_limited : 1;
+    uint32_t control_loop_time_error: 1;
+    uint32_t reset_watchdog_independent: 1;
+    uint32_t reset_watchdog_window: 1;
+    uint32_t reset_low_power: 1;
+    uint32_t reset_software: 1;
+    uint32_t reset_pin: 1;
     uint32_t reserved : 14;
 
     float vel_setpoint;
@@ -145,11 +154,12 @@ typedef struct MotorResponse_Motion_Packet {
 assert_size(MotorResponse_Motion_Packet, 48);
 
 typedef struct MotorResponsePacket {
-    MotorResponsePacketType type __attribute__((aligned (4)));
-    uint32_t crc __attribute__((aligned (4)));
+    MotorResponsePacketType type;
+    uint8_t reserved[3];
+    uint32_t crc;
     union ResponseData {
         MotorResponse_Params_Packet params;
         MotorResponse_Motion_Packet motion;
-    } data __attribute__((aligned (4)));
+    } data;
 } __attribute__((packed)) MotorResponsePacket;
 assert_size(MotorResponsePacket, 56);
