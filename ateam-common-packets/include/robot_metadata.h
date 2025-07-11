7bd228a28f0de016fd1b36b50bf054a9c4dd9de4
Will Stuckey <guyfleeman@gmail.com>
2025-07-11 18:35:14 -0400

    Life Before Death (#25)
    * initial commit of robot names
    
    * make metadata const ptr, and make name convention compliant
    
    * move weekly check against nightly to 24.04
    
    * Update ateam-common-packets/include/robot_metadata.h
    
    ---------
    
    Co-authored-by: Joe Spall <jspall0327@gmail.com>

---
+++ ateam-common-packets/include/robot_metadata.h
@@ -0,0 +1,18 @@
+const char *const ROBOT_NAMES[] = {
+    "Kaladin",
+    "Shallan",
+    "Dalinar",
+    "Navani",
+    "Szeth",
+    "Teft",
+    "Lift",
+    "Venli",
+    "Sylphrena",
+    "Pattern",
+    "The Stormfather",
+    "The Sibling",
+    "Auxiliary",
+    "Phenadorana",
+    "Wyndle",
+    "Timbre"
+};
