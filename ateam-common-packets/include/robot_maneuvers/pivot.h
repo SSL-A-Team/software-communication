5b0c64fabb26156ae38a9e415f3472e65f3324c4
nickwitten <59176907+nickwitten@users.noreply.github.com>
2026-06-21 13:13:18 -0700

    Pivot and Rust Lib Updates (#37)
    * adds safety check for pivot packet in rust lib
    
    * implements traits for ManeuverCommand enum in rust lib
    
    * pivot param changes

--- ateam-common-packets/include/robot_maneuvers/pivot.h
+++ ateam-common-packets/include/robot_maneuvers/pivot.h
@@ -3,15 +3,13 @@
 #include "../common.h"
 
 typedef struct PivotCommand {
-    float global_x_center;
-    float global_y_center;
     float global_theta;
     float max_angular_vel;
     float max_angular_acc;
     float orbit_radius;
-    float heading_lag;
+    float inset_angle;
 } PivotCommand;
-assert_size(PivotCommand, 28);
+assert_size(PivotCommand, 20);
 
 typedef struct PivotTelemetry {
     uint32_t reserved;
@@ -21,4 +19,4 @@ assert_size(PivotTelemetry, 4);
 typedef struct ExtendedPivotTelemetry {
     PivotCommand cmd_echo;
 } ExtendedPivotTelemetry;
-assert_size(ExtendedPivotTelemetry, 28);
+assert_size(ExtendedPivotTelemetry, 20);
