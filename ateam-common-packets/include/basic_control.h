2f3060c52bebe3f8fd82f184685662429efd59ae
Will Stuckey <guyfleeman@gmail.com>
2026-06-28 05:46:52 -0400

    add ESTOP_BRAKE mode

--- ateam-common-packets/include/basic_control.h
+++ ateam-common-packets/include/basic_control.h
@@ -12,13 +12,14 @@
 
 typedef enum BodyControlMode : uint8_t {
     BCM_OFF = 0,
-    BCM_GLOBAL_POSITION = 1,
-    BCM_GLOBAL_VELOCITY = 2,
-    BCM_LOCAL_VELOCITY = 3,
-    BCM_GLOBAL_ACCEL = 4,
-    BCM_LOCAL_ACCEL = 5,
-    BCM_HEADING_PIVOT = 6,
-    BCM_POINT_PIVOT = 7
+    BCM_ESTOP_BRAKE = 1,
+    BCM_GLOBAL_POSITION = 10,
+    BCM_GLOBAL_VELOCITY = 11,
+    BCM_LOCAL_VELOCITY = 12,
+    BCM_GLOBAL_ACCEL = 13,
+    BCM_LOCAL_ACCEL = 14,
+    BCM_HEADING_PIVOT = 20,
+    BCM_POINT_PIVOT = 21
     // add additional maneuvers and modes here
 } BodyControlMode;
 assert_size(BodyControlMode, 1);
